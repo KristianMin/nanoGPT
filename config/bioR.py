a7d9c60e854d94a66570700a91e144e2c217b709
KristianMin <kristian.minchev@insait.ai>
2024-12-06 12:58:31 +0000

    changed some parameters to accomodate hala

--- config/bioR.py
+++ config/bioR.py
@@ -15,29 +15,29 @@ wandb_run_name = 'mini-gpt'
 
 dataset = 'bioR'
 gradient_accumulation_steps = 1
-batch_size = 64
+batch_size = 96
 block_size = 256 # context of up to 256 previous characters
 
 # baby GPT model :)
-n_layer = 6
-n_head = 6
-n_embd = 384
-dropout = 0.2
+# n_layer = 6
+# n_head = 6
+# n_embd = 384
+# dropout = 0.2
 
 # Allen-Zhu's GPT
-# n_layer = 12
-# n_head = 20
-# n_embd = 64
-# dropout = 0.2
+n_layer = 12
+n_head = 32
+n_embd = 64
+dropout = 0.2
 
 learning_rate = 1e-3 # with baby networks can afford to go a bit higher
 max_iters = 5000
-lr_decay_iters = 5000 # make equal to max_iters usually
+lr_decay_iters = 1000 # make equal to max_iters usually
 min_lr = 1e-4 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 
-warmup_iters = 100 # not super necessary potentially
+warmup_iters = 1000 # not super necessary potentially
 
 # on macbook also add
-device = 'cpu'  # run on cpu only
-compile = False # do not torch compile the model
+# device = 'cpu'  # run on cpu only
+# compile = False # do not torch compile the model
